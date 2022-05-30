b515768428fc040f31debf47eaeee96705c6b07d
Mariella <mariella1990998@gmail.com>
2022-05-30 18:09:04 +0100

    exclude the edf_corrupted files flag

--- processing/BandPower_perWindow_NaNs.py
+++ processing/BandPower_perWindow_NaNs.py
@@ -62,8 +62,6 @@ subject = "909"
 # Set the root directory for patient
 root = os.path.join(paths.IN_EDF_DATA, subject)
 
-corrupted_edf_paths = paths.corrupted_edfs[subject]
-
 error_edfs = paths.error_edfs # channels labels appear in error edfs
 min_n_Chan = paths.min_n_Chan # the minimum threshold of the number of channels needed to be included in the edf file
 
@@ -79,7 +77,6 @@ EEG_channel_list = [element['name'] for element in Channels_json]
 # Get info about edf files and a list with the final paths pointed to the edf files to be used for the analysis
 [f_paths_clean, f_path_list_excluded, f_path_list_checkChanNotInList, f_paths, edf_chan] = clean_edf_paths(root = root,
                                                                                                            error_edfs = error_edfs,
-                                                                                                           corrupted_edf_paths = corrupted_edf_paths,
                                                                                                            channel_list = EEG_channel_list,
                                                                                                            min_n_Chan = min_n_Chan)
 edfs_info = get_EDFs_info(root = root,
